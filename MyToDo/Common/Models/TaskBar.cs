41e38fa7fa903d44b8b6e087df18bc854bb5d739
shawn <13382236+shao-lixin@user.noreply.gitee.com>
2026-03-08 16:31:56 +0800

    首页页面布局

---
+++ MyToDo/Common/Models/TaskBar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyToDo.Common.Models
+{
+    public class TaskBar:BindableBase
+    {
+        private string icon;
+        public string Icon
+        {
+            get => icon;
+            set
+            {
+                icon = value;
+            }
+        }
+        private string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+            }
+        }
+
+        private string content;
+        public string Content
+        {
+            get => content;
+            set
+            {
+                content = value;
+            }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        private int target;
+
+        public int Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+
+    }
+}
--- MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using MyToDo.Common.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace MyToDo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel:BindableBase
     {
+
+        public IndexViewModel()
+        {
+            CreateTaskBars();
+        }
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTaskBars()
+        {
+            TaskBars = new ObservableCollection<TaskBar>
+            {
+                new TaskBar{Icon="ClockFast",Title="汇总",Content="待办事项内容",Color="#FF0AC0FF",Target=0},
+                new TaskBar{Icon="ClockCheckOuline",Title="已完成",Content="日程安排内容",Color="#FF0078D7",Target=1},
+                new TaskBar{Icon="ChartLineVariant",Title="完成率",Content="100%",Color="#FF02C6DC",Target=2},
+                new TaskBar{Icon="PlaylistStar",Title="备忘录",Content="联系人内容",Color="#FFFFA000",Target=3},
+            };
+        }   
+
     }
 }
